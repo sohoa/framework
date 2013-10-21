c01169bf1c5fe42279edf8436dd1c0c1c0b44977
Camael24 <thehawk@hoa-project.net>
2013-10-21 17:13:06 +0200

    Resolve bugfix on composer.json autoload , and typo on Configuration.php and update Protocol on Framework.php and autoload error

--- Framework.php
+++ Framework.php
@@ -1,111 +1,123 @@
 <?php
-    namespace {
-        from('Hoa')
-            ->import('File.Read')
-            ->import('Router.Http')
-            ->import('Dispatcher.Basic');
+namespace {
+    from('Hoa')
+        ->import('File.Read')
+        ->import('Router.Http')
+        ->import('Dispatcher.Basic');
 
-        from('Sohoa')
-            ->import('Framework.Configuration');
-    }
-    namespace Sohoa\Framework {
+    from('Sohoa')
+        ->import('Framework.Configuration');
+}
+namespace Sohoa\Framework {
+
+    /**
+     * Class Framework
+     *
+     * @package Sohoa\Framework
+     */
+    class Framework
+    {
 
         /**
-         * Class Framework
-         *
-         * @package Sohoa\Framework
+         * @var Configuration object
          */
-        class Framework
+        protected $_parameters = null;
+
+        /**
+         * @var \Hoa\Router\Router|string
+         */
+        public $router = null;
+
+        /**
+         * @var \Hoa\Dispatcher\Dispatcher|string
+         */
+        public $dispatcher = null;
+
+        /**
+         * @var \Hoa\View\Viewable|string
+         */
+        public $view = null;
+
+        /**
+         *
+         * On définie le router , le dispatcher, et la vue avec les valeurs contenues dans le fichier de configuration ,
+         * ou par des valeurs par défault.
+         *
+         * Router       : bootstrap.router.handler
+         * Dispatcher   : bootstrap.dispatcher.handler
+         * View         : est définie manuellement par l'utilisateur
+         *
+         * Utiliser la valeur bootstrap.configfile pour changer le fichier de configuration chargé automatiquement
+         *
+         * @param array $parameter
+         */
+        public function __construct(Array $parameter = array())
         {
-
-            /**
-             * @var Configuration object
-             */
-            protected $_parameters = null;
-
-            /**
-             * @var \Hoa\Router\Router|string
-             */
-            public $router = null;
-
-            /**
-             * @var \Hoa\Dispatcher\Dispatcher|string
-             */
-            public $dispatcher = null;
-
-            /**
-             * @var \Hoa\View\Viewable|string
-             */
-            public $view = null;
-
-            /**
-             *
-             * On définie le router , le dispatcher, et la vue avec les valeurs contenues dans le fichier de configuration ,
-             * ou par des valeurs par défault.
-             *
-             * Router       : bootstrap.router.handler
-             * Dispatcher   : bootstrap.dispatcher.handler
-             * View         : est définie manuellement par l'utilisateur
-             *
-             * Utiliser la valeur bootstrap.configfile pour changer le fichier de configuration chargé automatiquement
-             *
-             * @param array $parameter
-             */
-            public function __construct(Array $parameter = array())
-            {
-                try {
-                    $this->_parameters = new Configuration($parameter);
-                    $router            = $this->_parameters->getParameter('bootstrap.router.handler');
-                    $this->router      = ($router === null) ? '\Hoa\Router\Http' : $router;
-                    $dispatcher        = $this->_parameters->getParameter('bootstrap.dispatcher.handler');
-                    $this->dispatcher  = ($dispatcher === null) ? '\Hoa\Dispatcher\Basic' : $dispatcher;
+            try {
+                $this->_parameters = new Configuration($parameter);
+                $core              = \Hoa\Core\Core::getInstance();
+                $parameter         = $core->getParameters();
+                $router            = $this->getParameters()->getParameter('bootstrap.router.handler');
+                $this->router      = ($router === null) ? '\Hoa\Router\Http' : $router;
+                $dispatcher        = $this->getParameters()->getParameter('bootstrap.dispatcher.handler');
+                $this->dispatcher  = ($dispatcher === null) ? '\Hoa\Dispatcher\Basic' : $dispatcher;
 
 
-                } catch (\Hoa\Core\Exception $e) {
-                    var_dump($e->getFormattedMessage());
+                if ($parameter instanceof Parameter) {
+
+                    $parameter->setParameter('protocol.Application', '(:cwd:h:)/Application');
+                    $parameter->setParameter('protocol.Public', '(:%root.application:)/Public');
+                    $parameter->setParameter('namespace.prefix.Application', '(:cwd:h:)/');
+
                 }
-
-            }
-
-            /**
-             * @param string $file
-             */
+                $core->setProtocol();
 
 
-            /**
-             * Get Default parameters.
-             *
-             * @access  public
-             * @return  \Hoa\Core\Parameter
-             */
-            public function getParameters()
-            {
-                return $this->_parameters->getParameters();
-            }
-
-            public function run()
-            {
-                try {
-
-                    $dispatcher = $this->dispatcher;
-                    $router     = $this->router;
-                    $view       = $this->view;
-                    if (is_string($router))
-                        $router = dnew($router);
-
-                    if (is_string($dispatcher))
-                        $dispatcher = dnew($dispatcher);
-
-                    if (is_string($view))
-                        $view = dnew($view);
-
-                    $dispatcher->dispatch($router, $view);
-
-                } catch (\Hoa\Core\Exception $e) {
-                    var_dump($e->getFormattedMessage());
-                }
-
+            } catch (\Hoa\Core\Exception $e) {
+                var_dump($e->getFormattedMessage());
             }
 
         }
+
+        /**
+         * @param string $file
+         */
+
+
+        /**
+         * Get Default parameters.
+         *
+         * @access  public
+         * @return  \Hoa\Core\Parameter
+         */
+        public function getParameters()
+        {
+            return $this->_parameters->getParameters();
+        }
+
+        public function run()
+        {
+            try {
+
+                $dispatcher = $this->dispatcher;
+                $router     = $this->router;
+                $view       = $this->view;
+                if (is_string($router))
+                    $router = dnew($router);
+
+                if (is_string($dispatcher))
+                    $dispatcher = dnew($dispatcher);
+
+                if (is_string($view))
+                    $view = dnew($view);
+
+                $dispatcher->dispatch($router, $view);
+
+            } catch (\Hoa\Core\Exception $e) {
+                var_dump($e->getFormattedMessage());
+            }
+
+        }
+
     }
+}
