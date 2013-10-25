e02be0bdafa46d207f6c0e2e38f2abe576670ae7
Camael24 <thehawk@hoa-project.net>
2013-10-25 17:54:06 +0200

    Simplification of Framework and remove Configuration its become useless, add of unit test for Framework Change Code Style

--- Classes/Framework.php
+++ Classes/Framework.php
@@ -2,104 +2,76 @@
 
 namespace Sohoa\Framework {
 
-    use Hoa\Core\Parameter\Parameter;
+
+    use Hoa\Core\Core;
+    use Hoa\Dispatcher\Basic;
+
 
     /**
-    * Class Framework
-    *
-    * @package Sohoa\Framework
-    */
+     * Class Framework
+     *
+     * @package Sohoa\Framework
+     */
     class Framework
     {
         /**
-         * @var Configuration object
-         */
-        protected $_parameters = null;
-
-        /**
-         * @var \Hoa\Router\Router|string
+         * @var Router
          */
         public $router = null;
 
         /**
-         * @var \Hoa\Dispatcher\Dispatcher|string
+         * @var \Hoa\Dispatcher\Basic
          */
         public $dispatcher = null;
 
         /**
-         * @var \Hoa\View\Viewable|string
+         * @var \Hoa\View\Viewable
          */
         public $view = null;
 
         /**
          *
-         * On définie le router , le dispatcher, et la vue avec les valeurs contenues dans le fichier de configuration ,
-         * ou par des valeurs par défault.
+         * On définie le router , le dispatcher, et la vue
          *
          * Router       : bootstrap.router.handler
          * Dispatcher   : bootstrap.dispatcher.handler
          * View         : est définie manuellement par l'utilisateur
          *
-         * Utiliser la valeur bootstrap.configfile pour changer le fichier de configuration chargé automatiquement
-         *
-         * @param array $parameter
          */
-        public function __construct(Array $parameter = array()) {
+
+        public function __construct()
+        {
 
             try {
-
-                $this->_parameters = new Configuration($parameter);
-                $core              = \Hoa\Core\Core::getInstance();
-                $parameter         = $core->getParameters();
-                $router            = $this->getParameters()->getParameter('bootstrap.router.handler');
-                $this->router      = ($router === null) ? '\Hoa\Router\Http' : $router;
-                $dispatcher        = $this->getParameters()->getParameter('bootstrap.dispatcher.handler');
-                $this->dispatcher  = ($dispatcher === null) ? '\Hoa\Dispatcher\Basic' : $dispatcher;
+                $core             = Core::getInstance();
+                $parameter        = $core->getParameters();
+                $this->router     = new Router();
+                $this->dispatcher = new Basic();
 
                 if ($parameter instanceof Parameter) {
 
-                  $parameter->setParameter('protocol.Application', '(:cwd:h:)/Application/');
-                  $parameter->setParameter('protocol.Public', '(:%root.application:)/Public/');
-                  $parameter->setParameter('namespace.prefix.Application', '(:cwd:h:)/');
+                    $parameter->setParameter('protocol.Application', '(:cwd:h:)/Application/');
+                    $parameter->setParameter('protocol.Public', '(:%root.application:)/Public/');
+                    $parameter->setParameter('namespace.prefix.Application', '(:cwd:h:)/');
                 }
                 $core->setProtocol();
 
-
+                if (file_exists('hoa://Application/Config/Route.php')) {
+                    require_once 'hoa://Application/Config/Route.php';
+                }
             } catch (\Hoa\Core\Exception $e) {
 
                 var_dump($e->getFormattedMessage());
             }
         }
 
-        /**
-         * Get Default parameters.
-         *
-         * @access  public
-         * @return  \Hoa\Core\Parameter
-         */
-        public function getParameters() {
+        public function run()
+        {
 
-            return $this->_parameters->getParameters();
-        }
-
-        public function run() {
 
             try {
 
-                $dispatcher = $this->dispatcher;
-                $router     = $this->router;
-                $view       = $this->view;
-                if (is_string($router))
-                    $router = dnew($router);
-
-                if (is_string($dispatcher))
-                    $dispatcher = dnew($dispatcher);
-
-                if (is_string($view))
-                    $view = dnew($view);
-
-                $dispatcher->dispatch($router, $view);
-
+                $this->dispatcher->dispatch($this->router, $this->view);
             } catch (\Hoa\Core\Exception $e) {
 
                 var_dump($e->getFormattedMessage());
---
+++ Tests/Unit/Framework.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Sohoa\Framework\Tests\Unit;
+
+require_once __DIR__ . '/Runner.php';
+
+class Framework extends \atoum\test
+{
+
+    public function testInit()
+    {
+        $fwk        = new \Sohoa\Framework\Framework();
+        $router     = $fwk->router;
+        $dispatcher = $fwk->dispatcher;
+
+        $this->object($router)
+            ->isInstanceOf('\Sohoa\Framework\Router');
+
+        $this->object($dispatcher)
+            ->isInstanceOf('\Hoa\Dispatcher\Basic');
+    }
+}
